ad7dd32dbdad2b28c4578bb386c9010008af2ed9
Sivangula Mani Harika <sivangulamaniharika@gmail.com>
2026-07-08 04:34:00 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (86.45%)

---
+++ 1635-number-of-good-pairs/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        counter=Counter(nums)
+        gp=0
+        for num in nums:
+            counter[num]-=1
+            gp+=counter[num]
+        return gp
+
+        
